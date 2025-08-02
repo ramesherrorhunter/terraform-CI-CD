ae238f338d597412e4b423c1ee9e7aaa53158274
tracebackerror_ramesh <rahulrajput81412@gmail.com>
2025-08-02 19:59:00 +0530

    Added main and var tf code

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_role" "example_role" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "example_policy" {
+  name = "example_policy"
+  role = aws_iam_role.example_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "role_name" {
+  description = "IAM role name"
+  type        = string
+}
